e94ad76f8a3aa01e28e3c672ce870bfaaa046393
mem <mem@asus.asus>
2021-11-12 16:31:18 +0000

    initial Unity Project

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    // Start() and Update() methods deleted - we don't need them right now
+
+    public static MainManager Instance;
+
+    public Color TeamColor; // new variable declared
+
+    [System.Serializable]
+    class SavaData
+    {
+        public Color TeamColor;
+    }
+    
+    public void SaveColor()
+    {
+        SavaData data = new SavaData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SavaData data = JsonUtility.FromJson<SavaData>(json);
+            TeamColor = data.TeamColor;
+        }
+    }
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+}
